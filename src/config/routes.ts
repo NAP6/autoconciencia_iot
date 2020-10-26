143911b466cbc2c5bcafac449f3a6defa4519a99
Jaime Panata <panatta3004@gmail.com>
2020-10-26 11:39:59 -0600

    Implementacion Interfaces

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -12,7 +12,7 @@ export default function (app: Application, upload: Multer) {
   //app.get("/self-awareness_processes", nv.loggedIn, nv.self-awareness_processes);
   app.get("/measurement_units", nv.loggedIn, nv.measurement_units);
   app.get("/scales", nv.loggedIn, nv.scales);
-  app.get("/decision", nv.loggedIn, nv.decision);
+  app.get("/decision_criteria", nv.loggedIn, nv.decision);
   app.get("/formulas", nv.loggedIn, nv.formulas);
   app.get("/functions", nv.loggedIn, nv.functions);
   //app.get("/service", nv.loggedIn, nv.service);
