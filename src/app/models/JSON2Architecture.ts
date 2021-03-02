56c0c8247cea52083aeeb663aa9076b4f1edd6ba
Nicolas <nicolas.doo@hotmail.com>
2021-03-02 16:37:31 -0500

    Se recreo la forma de sacar los elementos del modelo de arquitectura (modelos)

--- src/app/models/JSON2Architecture.ts
+++ src/app/models/JSON2Architecture.ts
@@ -1,19 +1,22 @@
 import { Entity } from "./architecture/Entity";
 import { DataFlow } from "./architecture/DataFlow";
 import { IoTSystem } from "./architecture/IoTSystem";
-//import { Property } from "./architecture/Property";
+import { Property } from "./architecture/Property";
 
 export class JSON2Architecture {
   private _json: Architecture;
   private _modelEntity: Entity[];
   private _modelDataFlow: DataFlow[];
-  private _modelIoTSystem: IoTSystem[];
+  private _modelIoTSystem: IoTSystem;
 
   constructor(json: Architecture) {
-    this._json = json;
+    this._json = json[Object.keys(json)[0]];
     this._modelEntity = [];
     this._modelDataFlow = [];
-    this._modelIoTSystem = [];
+    this._modelIoTSystem = new IoTSystem(-1, "");
+    this.extractSystems();
+    this.extractDataFlows();
+    this.extractEntitys();
   }
 
   get json(): Architecture {
@@ -40,18 +43,189 @@ export class JSON2Architecture {
     this._modelDataFlow = modelDataFlow;
   }
 
-  get modelIoTSystem(): IoTSystem[] {
+  get modelIoTSystem(): IoTSystem {
     return this._modelIoTSystem;
   }
 
-  set modelIoTSystem(modelIoTSystem: IoTSystem[]) {
+  set modelIoTSystem(modelIoTSystem: IoTSystem) {
     this.modelIoTSystem = modelIoTSystem;
   }
 
-  private extractSystems(){}
-  private extractEntitys(){}
-  private extractDataFlows(){}
-  private extractPropertys(){}
+  private extractSystems() {
+    var containsIoTSystem = this._json["containsIoTSystem"];
+    var system = new IoTSystem(containsIoTSystem.id, containsIoTSystem.name);
+    if (containsIoTSystem.containsIoTSubSystem) {
+      system.IoTSubSystem = this.extractSystems_recursive(
+        containsIoTSystem.containsIoTSubSystem
+      );
+    }
+    this._modelIoTSystem = system;
+  }
+
+  private extractSystems_recursive(system: any): IoTSystem[] {
+    var systeRe: IoTSystem[] = [];
+    system.forEach((subSystem) => {
+      var sub = new IoTSystem(subSystem.id, subSystem.name);
+      if (subSystem.containsIoTSubSystem) {
+        sub.IoTSubSystem = this.extractSystems_recursive(
+          subSystem.containsIoTSubSystem
+        );
+      }
+      systeRe.push(sub);
+    });
+    return systeRe;
+  }
+
+  private extractDataFlows() {
+    var containsDataFlow = this._json["containsDataFlow"];
+    containsDataFlow.forEach((flows) => {
+      var newFlow = new DataFlow(
+        flows.id,
+        flows.description,
+        flows.communicationType
+      );
+      this._modelDataFlow.push(newFlow);
+    });
+  }
+
+  private extractEntitys() {
+    var containsEntity = this._json["containsEntity"];
+    this.extractEntitys_recursive(containsEntity);
+  }
+
+  private extractEntitys_recursive(entitys: any): Entity[] {
+    var entitysRe: Entity[] = [];
+    entitys.forEach((entity) => {
+      var id = entity.id;
+      var name = entity.name;
+      var entityType = entity["xsi:type"].split(":")[1];
+      var newEntity = new Entity(id, name, entityType);
+
+      if (entity.isPartOf) {
+        this.matchPairs_SystemEntity(newEntity, entity.isPartOf);
+      }
+
+      // Inicio: Extraer de PhysicalEntity
+      if (
+        newEntity.entityType == "PhysicalEntity" ||
+        newEntity.entityType == "IoTUser" ||
+        newEntity.entityType == "HumanUser" ||
+        newEntity.entityType == "NonHumanUser"
+      ) {
+        if (entity.containsSubPhysicalEntity) {
+          newEntity.subEntity.concat(
+            this.extractSubPhysicalEntitys(entity.containsSubPhysicalEntity)
+          );
+        }
+        if (entity.containsComputingNode) {
+          newEntity.subEntity.concat(
+            this.extractEntitys_recursive(entity.containsComputingNode)
+          );
+        }
+      }
+      // Fin: Extraer de PhysicalEntity
+
+      // Inicio: Extraer de ComputingNode
+      if (
+        newEntity.entityType == "ComputingNode" ||
+        newEntity.entityType == "CloudNode" ||
+        newEntity.entityType == "FogNode" ||
+        newEntity.entityType == "EdgeNode" ||
+        newEntity.entityType == "IoTDevice" ||
+        newEntity.entityType == "IoTGateway" ||
+        newEntity.entityType == "Sensor" ||
+        newEntity.entityType == "Tag" ||
+        newEntity.entityType == "Actuator"
+      ) {
+        if (entity.containsResource) {
+          newEntity.subEntity.concat(
+            this.extractEntitys_recursive(entity.containsResource)
+          );
+        }
+      }
+      // Fin: Extraer de ComputingNode
+
+      if (entity.hasProperty) {
+        newEntity.propertys = this.extractPropertys(entity.hasProperty);
+      }
+      entitysRe.push(newEntity);
+    });
+    return entitysRe;
+  }
+
+  private extractSubPhysicalEntitys(entitys: any): Entity[] {
+    var entitysRe: Entity[] = [];
+    entitys.forEach((entity) => {
+      var id = entity.id;
+      var name = entity.name;
+      var entityType = "PhysicalEntity";
+      var newEntity = new Entity(id, name, entityType);
+
+      if (entity.isPartOf) {
+        this.matchPairs_SystemEntity(newEntity, entity.isPartOf);
+      }
+
+      if (entity.containsSubPhysicalEntity) {
+        newEntity.subEntity.concat(
+          this.extractSubPhysicalEntitys(entity.containsSubPhysicalEntity)
+        );
+      }
+      if (entity.containsComputingNode) {
+        newEntity.subEntity.concat(
+          this.extractEntitys_recursive(entity.containsComputingNode)
+        );
+      }
+      if (entity.hasProperty) {
+        newEntity.propertys = this.extractPropertys(entity.hasProperty);
+      }
+      entitysRe.push(newEntity);
+    });
+    return entitysRe;
+  }
+
+  private matchPairs_SystemEntity(entity: Entity, isPartOf: string) {
+    var listRoutes = isPartOf.split(" ");
+    listRoutes.forEach((route) => {
+      var system: IoTSystem = this._modelIoTSystem;
+      var routeAux: string[] = route.substring(1, route.length).split("/@");
+      routeAux = routeAux.splice(2, routeAux.length);
+      routeAux.forEach((stop) => {
+        var index: number = parseInt(stop.split(".")[1]);
+        system = system.IoTSubSystem[index];
+      });
+      entity.iotSystem.push(system);
+      system.entity.push(entity);
+    });
+  }
+
+  private extractPropertys(propertys: any): Property[] {
+    var propertysRe: Property[] = [];
+    propertys.forEach((prop) => {
+      var newProperty = new Property(prop.id, prop.name);
+      if (prop.hasRulePropertyToDataColumn) {
+        this.matchPairs_PropertyDataFlow(
+          newProperty,
+          prop.hasRulePropertyToDataColumn
+        );
+      }
+      propertysRe.push(newProperty);
+    });
+    return propertysRe;
+  }
+
+  private matchPairs_PropertyDataFlow(
+    property: Property,
+    RuleProperty: string
+  ) {
+    var listRoutes = RuleProperty.split(" ");
+    listRoutes.forEach((route) => {
+      var routeAux: string = route.substring(1, route.length).split("/@")[1];
+      var index: number = parseInt(routeAux.split(".")[1]);
+      var flow = this._modelDataFlow[index];
+      property.dataFlow.push(flow);
+      flow.propertys.push(property);
+    });
+  }
 }
 
 interface Architecture {}
