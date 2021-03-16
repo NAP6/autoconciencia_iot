7d20d427830f9be5db4999ecdf9020ba8a9f84b1
Jaime Panata <panatta3004@gmail.com>
2021-03-15 22:25:56 -0600

    Unidades de medida Listo

---
+++ src/app/controller/page_contollers/measurement_units.ts
@@ -0,0 +1,88 @@
+import { json } from "body-parser";
+import { Console } from "console";
+import { Request, Response, NextFunction } from "express";
+import { database2 } from "../../data/database2";
+import { MeasurementUnitQ } from "../../models/selfAwarness/qwertyModels/MeasurementUnitQ";
+
+export function measurement_units(req: Request, res: Response) {
+  res.render("measurement_units", {
+    error: req.flash("error"),
+    succes: req.flash("succes"),
+    session: req.session,
+  });
+}
+export async function get_measurement_units(req: Request, res: Response) {
+  if (req.session?.user) {
+    var db = new database2();
+    var measurement:MeasurementUnitQ=new MeasurementUnitQ(-1,"","","");
+    var rows= await db.qwerty(measurement.toSqlSelect([],[]));
+    res.json(rows);
+    
+  } else {
+    res.json({ error: "debe iniciar session para poder usar la api" });
+  }
+}
+export async function add_measurement_units(req: Request, res: Response) {
+  if(req.session?.user){
+    var db = new database2();
+    var newMeasurement = req.body;
+    var measurement: MeasurementUnitQ = new MeasurementUnitQ(
+      -1,
+      newMeasurement.name,
+      newMeasurement.description,
+      newMeasurement.acronym,
+    );
+    measurement.active = newMeasurement.active;
+    await db.qwerty(
+        measurement.toSqlInsert(
+        [],
+        []
+      )
+    );
+    res.json({ Mensaje: "Los datos se han enviado con exito" });
+  } else {
+    res.json({ error: "debe iniciar session para poder usar la api" });
+  }
+}
+export async function upd_measurement_units(req: Request, res: Response) {
+  if(req.session?.user){
+    var db = new database2();
+    var newMeasurement = req.body;
+    var measurement: MeasurementUnitQ = new MeasurementUnitQ(
+        newMeasurement.id,
+        newMeasurement.name,
+        newMeasurement.description,
+        newMeasurement.acronym,
+    );
+    measurement.active = measurement.active;
+    await db.qwerty(
+        newMeasurement.toSqlUpdate(
+        [],
+        []
+      )
+    );
+    res.json({ Mensaje: "Los datos se han enviado con exito" });
+  } else {
+    res.json({ error: "debe iniciar session para poder usar la api" });
+  }
+}
+export async function del_measurement_units(req: Request, res: Response) {
+  if(req.session?.user){
+    var db = new database2();
+    var newMeasurement = req.body;
+    var measurement: MeasurementUnitQ = new MeasurementUnitQ(
+        newMeasurement.id,
+      "",
+      "",
+      "",
+    );
+    await db.qwerty(
+      measurement.toSqlDelete(
+       []
+      )
+    );
+    res.json({ Mensaje: "Los datos se han enviado con exito" });
+  } else {
+    res.json({ error: "debe iniciar session para poder usar la api" });
+  }
+}
---
+++ src/app/models/selfAwarness/qwertyModels/MeasurementUnitQ.ts
@@ -0,0 +1,51 @@
+import{MeasurementUnit} from "../MeasurementUnit";
+import{ SQL_Qwerty } from "../../SQL_Qwerty";
+export class MeasurementUnitQ extends MeasurementUnit implements SQL_Qwerty{
+    toSqlInsert(tag:string[],value:string[]):string{
+var sql=`INSERT INTO
+        unidadmedicion (
+                um_nombre,
+                um_descripcion,
+                um_acronimo,
+                um_activo
+            ) VALUES (
+                '${this.name}',
+                '${this.description}',
+                '${this.acronym}',
+                '${this.active ? 1: 0}',
+
+            )`;
+return sql;
+    }
+   toSqlSelect(tag:string[],value:string[]): string {
+    var sql = `SELECT 
+    um_id as id,
+    um_nombre as name,
+    um_descripcion as description,
+    um_acronimo as acronym,
+    um_activo as active
+         FROM 
+         unidadmedicion`;
+return sql;
+}
+toSqlDelete(value: string[]): string {
+    var sql=`DELETE FROM unidadmedicion WHERE um_id=${this.id} `;
+    return sql;
+}
+
+toSqlUpdate(tag: string[], value: string[]): string {
+    var sql = `UPDATE 
+    unidadmedicion
+    SET
+    um_nombre='${this.name}', 
+    um_descripcion='${this.description}',
+    um_acronimo='${this.acronym}',
+    met_activo='${this.active ? 1 : 0}'
+  WHERE 
+    um_id=${this.id}`;
+return sql;
+}
+toObjectArray(rows: any): any[] {
+    throw new Error("Method not implemented.");
+}
+}
